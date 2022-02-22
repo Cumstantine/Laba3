cee9f0786c470431c926024b6dd5faa9cbab1ca5
Cumstantine <99866981+Cumstantine@users.noreply.github.com>
2022-02-22 10:34:46 +0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,55 @@
+function start(){
+    document.getElementById("startText").classList.add("hideBlock");
+    document.getElementById("startButton").classList.add("hideBlock");
+    
+    document.getElementById("question").classList.remove("hideBlock");
+    document.getElementById("question").innerHTML = "Попытка" + stage + ": " + middle;
+    
+    
+      document.getElementById("small").classList.remove("hideBlock");
+      document.getElementById("bingo").classList.remove("hideBlock");
+      document.getElementById("big").classList.remove("hideBlock");
+}
+let min = 0;
+let max = 100;
+let middle = Math.floor((min + max)/2);
+let stage = 1;
+function checkGame(){
+    if(min == max){
+        document.getElementById("question").innerHTML = "Вы звгвдвли число" + min;
+        
+        document.getElementById("small").classList.add("hideBlock");
+      document.getElementById("bingo").classList.add("hideBlock");
+      document.getElementById("big").classList.add("hideBlock");
+        return;
+    }
+   stage += 1;
+    if (stage == 8){
+        document.getElementById("question").innerHTML = "Жульничать омерзительно, ничтожество";
+    }
+       document.getElementById("question").innerHTML = "Попытка" + stage + ": " + middle;
+}
+
+
+function smallclick(){
+    max = middle;
+    middle = Math.floor((min + max)/2);
+    if(max == middle){
+        middle-=1;
+    }
+    checkGame();
+}
+function bigclick(){
+    min = middle;
+    middle = Math.floor((min + max)/2);
+    if(min == middle){
+        middle +=1;
+    }
+    checkGame();
+}
+
+function bigclick(){
+    min = middle;
+    max = middle;
+    checkGame();
+}
